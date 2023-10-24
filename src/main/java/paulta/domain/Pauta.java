4d23b0d23ff96bed56f7bec148036b24a11af92c
welingtonalcantara <welingtonalcantara@gmail.com>
2023-10-24 19:10:30 -0300

    implementa domain pauta

---
+++ src/main/java/paulta/domain/Pauta.java
@@ -0,0 +1,23 @@
+package paulta.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@ToString
+@Entity
+public class Pauta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid", updatable = false, unique = true, nullable = false)
+    private UUID id;
+    private String titulo;
+    private String descricao;
+    private UUID idAssociadoAutor;
+    private LocalDateTime dataCriacao;
+
+}
